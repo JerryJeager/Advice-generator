f4fa10d567416951c8e360e7cd04972f2e5c4852
Amadi Jerry <amadijerry310@gmail.com>
2023-02-03 15:52:48 +0100

    made request to advice api with async and await

--- script.js
+++ script.js
@@ -1,49 +1,33 @@
-// const advice = new XMLHttpRequest()
-// advice.addEventListener('readystatechange', () => {
-//     if(advice.readyState == 4){
-//         console.log(advice.responseText)
-//     }
-// })
+const adviceNumber = document.querySelector('.advice-number').querySelector('p')
+const adviceQuote = document.querySelector('.advice-quote').querySelector('p')
+const changeAdviceBtn = document.querySelector('button')
 
-// advice.open('GET', 'https://api.adviceslip.com/advice')
-// advice.send()
 
-// const getAdvice = (request) => {
-//     return new Promise((resolve, reject) => {
-//         const advice = new XMLHttpRequest()
-//         advice.addEventListener('readystatechange', () => {
-//             if(advice.readyState == 4 && advice.status == 200){
-//                 const data = JSON.parse(advice.responseText)
-//                 resolve(data)
-//             }else if(advice.readyState == 4){
-//                 const err = 'error getting advice'
-//                 reject(err)
-//             }
-//         })
-//         advice.open('GET', request)
-//         advice.send()
-//     })
-// }
-
-// getAdvice('https://api.adviceslip.com/advice').then((data) => {
-//     console.log('resolved:', data)
-//     console.log(data.slip.id, data.slip.advice)
-// }).catch(err => {
-//     console.log('error:', err)
-// })
 
 const newAdvice = async (advice) => {
     const response = await fetch(advice)
-    if(response.status !== 200){
-        throw new Error('error getting advice')
+    if (response.status !== 200) {
+        throw new Error('Advice not found: ')
     }
     const data = response.json()
     return data
 
 }
 
-newAdvice('https://api.adviceslip.com/advice').then((data) => {
-    console.log('resolved', data)
-}).catch(err => {
-    console.log('error:', err)
-})
+function displayAdvice() {
+    newAdvice('https://api.adviceslip.com/advice').then((data) => {
+        adviceNumber.textContent = `ADVICE #${data.slip.id}`
+        adviceQuote.textContent = `"${data.slip.advice}"`
+        // console.log(data)
+    }).catch(err => {
+        adviceNumber.textContent = ``
+        adviceQuote.textContent = `${err.message} Check your internet connection`
+        adviceQuote.style.color = 'red'
+        adviceQuote.style.fontSize = '16px'
+    })
+}
+
+
+changeAdviceBtn.addEventListener('click', displayAdvice)
+window.addEventListener('load', displayAdvice)
+
